4343511816ac968efd0ce192c97a2372c2bfc034
adrian_sun <ziqingyi@qq.com>
2020-07-03 23:43:40 +1000

    Add project files.

---
+++ P04.Model/Food/AbstractFood.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.Model
+{
+    public abstract class AbstractFood
+    {
+        public BaseFood BaseFood { get; set; }
+
+        public AbstractFood()
+        {
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ P04.Model/Food/BaseFood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.Model
+{
+    public class BaseFood
+    {
+        #region Basic properties
+
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string FoodDescription { get; set; }
+        public string CookMethod { get; set; }
+        public decimal Price { get; set; }
+        public ConsoleColor MessageColor { get; set; }
+
+        #endregion
+
+    }
+}
