dcd24a67dba4a1c82553c9b6c3f6a11888d582b3
adrian_sun <ziqingyi@qq.com>
2020-07-04 22:18:58 +1000

    braised polk ball

--- P04.Model/Food/BraisedPolkBall.cs
+++ P04.Model/Food/BraisedPolkBall.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P04.Helper;
 
 namespace P04.Model.Food
 {
@@ -15,11 +16,8 @@ namespace P04.Model.Food
 
         public override void Cook()
         {
-
+            String des = string.Format("Now chief is cooking, your dish is : {0}", this.BaseFood.FoodName);
+            LogHelper.WirteInfoLog(des, this.BaseFood.MessageColor);
         }
-
-
-
-
     }
 }
