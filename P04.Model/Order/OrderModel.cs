ebf2c80b40a3a7b066ecc069f5d4c1501173b358
adrian sun <ziqingyi@qq.com>
2020-07-14 09:20:40 +1000

    OrderModel

---
+++ P04.Model/Order/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.Model.Order
+{
+    public class OrderModel
+    {
+        public string ScoreMessage { get; set; }
+        public List<string> FoodScoreMessage { get; set; }
+
+        public List<string> CustomerList { get; set; }
+    }
+}
